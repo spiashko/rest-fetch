37c9d73142e55eb722e05755df0ee7e190e2f2a3
Siarhei Piashko <peshko.sergey@gmail.com>
2022-02-06 23:19:17 +0100

    update jackson module name

--- jackson-support/src/main/java/com/spiashko/jpafetch/jacksonjpa/autoconfigure/JacksonConfiguration.java
+++ jackson-support/src/main/java/com/spiashko/jpafetch/jacksonjpa/autoconfigure/JacksonConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JacksonJpaConfiguration {
+public class JacksonConfiguration {
 
     @ConditionalOnMissingBean
     @Bean
